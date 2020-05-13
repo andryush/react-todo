a99f8be73e74f309130dcaf645e082d2eecbde82
andryush <andryush333@gmail.com>
2020-05-13 04:25:32 +0400

    Updates

---
+++ precache-manifest.b45099985980f38d6dac56380eb114cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4626325bd6147c3e2e27bdc1822fa5",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "cfb6dc742dd837498ac9",
+    "url": "/react-todo/static/css/main.c7af9b61.chunk.css"
+  },
+  {
+    "revision": "754a3cb6be336225695f",
+    "url": "/react-todo/static/js/2.2d893c11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.2d893c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb6dc742dd837498ac9",
+    "url": "/react-todo/static/js/main.aa77200a.chunk.js"
+  },
+  {
+    "revision": "a7fb28e01dcc192be58f",
+    "url": "/react-todo/static/js/runtime-main.59b49ada.js"
+  }
+]);
